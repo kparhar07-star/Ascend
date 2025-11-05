9279a5f92247cd7aa9f1415e25806e5af49dc316
kparhar07-star <kparhar07@gmail.com>
2025-11-04 20:38:36 -0800

    Journal page

--- Ascend/src/App.jsx
+++ Ascend/src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./Components/Navigation/Navigation.jsx";
+import Journal from "./Pages/Journal/Journal.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navigation />
-    </BrowserRouter>  
+
+        <Navigation />
+          <Routes>
+            <Route path="/journal" element={<Journal />} />
+          </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
